e54cad48cff780410009f29c464e9051e634cf21
osapon <osa@osa-p.net>
2023-02-04 17:32:04 +0900

    Create j_manga_history_view.meta.js

---
+++ j_manga_history_view.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         ジャンプ系サイト履歴全部表示
+// @namespace    osa-p.net
+// @author       osa <osa@osa-p.net>
+// @version      1.0.0
+// @homepageURL  https://github.com/osapon/j_manga_history_view
+// @match        https://tonarinoyj.jp/history
+// @match        https://shonenjumpplus.com/history
+// @match        https://comic-ogyaaa.com/history
+// @updateURL    https://github.com/osapon/j_manga_history_view/raw/master/j_manga_history_view.meta.js
+// @downloadURL  https://github.com/osapon/j_manga_history_view/raw/master/j_manga_history_view.user.js
+// ==/UserScript==
