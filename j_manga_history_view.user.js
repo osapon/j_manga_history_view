efa6c9e60acdc5f1522c0833ca831039406d224c
osapon <osa@osa-p.net>
2024-04-17 22:20:22 +0900

    対応サイト追加

--- j_manga_history_view.user.js
+++ j_manga_history_view.user.js
@@ -2,11 +2,13 @@
 // @name         ジャンプ系サイト履歴全部表示
 // @namespace    osa-p.net
 // @author       osa <osa@osa-p.net>
-// @version      1.0.0
+// @version      1.0.1
 // @homepageURL  https://github.com/osapon/j_manga_history_view
 // @match        https://tonarinoyj.jp/history
 // @match        https://shonenjumpplus.com/history
 // @match        https://comic-ogyaaa.com/history
+// @match        https://www.sunday-webry.com/history
+// @match        https://comic-days.com/my/history
 // @updateURL    https://github.com/osapon/j_manga_history_view/raw/master/j_manga_history_view.meta.js
 // @downloadURL  https://github.com/osapon/j_manga_history_view/raw/master/j_manga_history_view.user.js
 // ==/UserScript==
